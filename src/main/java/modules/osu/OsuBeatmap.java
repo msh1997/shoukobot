fe91e94454d5a67ca6d505270d44d446fb0d9c5f
msh1997 <michaelwsx@gmail.com>
2019-05-20 01:40:19 -0400

    adding beatmap and beatmapset objects

--- src/main/java/modules/osu/OsuBeatmap.java
+++ src/main/java/modules/osu/OsuBeatmap.java
@@ -3,5 +3,47 @@ package modules.osu;
 public class OsuBeatmap {
 
     private int beatmapId;
-    private int sr;
+    private double sr;
+    private double od;
+    private double ar;
+    private double bpm;
+    private double hp;
+    private double cs;
+    private String diffName;
+
+    public OsuBeatmap(int beatmapId) {
+        this.beatmapId = beatmapId;
+    }
+
+    public int getBeatmapId() {
+        return beatmapId;
+    }
+
+    public double getSr() {
+        return sr;
+    }
+
+    public double getOd() {
+        return od;
+    }
+
+    public double getAr() {
+        return ar;
+    }
+
+    public double getBpm() {
+        return bpm;
+    }
+
+    public double getHp() {
+        return hp;
+    }
+
+    public double getCs() {
+        return cs;
+    }
+
+    public String getDiffName() {
+        return diffName;
+    }
 }
--- src/main/java/modules/osu/OsuBeatmapSet.java
+++ src/main/java/modules/osu/OsuBeatmapSet.java
@@ -1,4 +1,26 @@
 package modules.osu;
 
+import java.util.List;
+
 public class OsuBeatmapSet {
+
+    private int id;
+    private List<OsuBeatmap> beatmapList;
+    private String mapper;
+
+    public OsuBeatmapSet(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<OsuBeatmap> getBeatmapList() {
+        return beatmapList;
+    }
+
+    public String getMapper() {
+        return mapper;
+    }
 }
