8fa02a4aa3ce593f88671f0c2546c8db7c3f87d9
msh1997 <michaelwsx@gmail.com>
2019-05-19 23:11:03 -0400

    added osu remove tracking functionality and writing to json file

--- src/main/java/modules/osu/OsuEventHandler.java
+++ src/main/java/modules/osu/OsuEventHandler.java
@@ -8,6 +8,7 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +25,6 @@ public class OsuEventHandler {
             JSONObject obj = (JSONObject)jsonParser.parse(reader);
 
             JSONArray usersJsonArray = (JSONArray)(obj.get("users"));
-            System.out.println(usersJsonArray);
 
             usersJsonArray.forEach( user -> osuUsersList.add(new OsuUser((String)user)));
 
@@ -38,31 +38,79 @@ public class OsuEventHandler {
 
     }
 
+    public void addUserToTracking(String[] messages, MessageReceivedEvent event){
+
+        String username = "";
+
+        for(int i = 2; i < messages.length; i++){
+            username += messages[i];
+        }
+
+        osuUsersList.add(new OsuUser(username));
+        writeTrackedUsersJson();
+        event.getChannel().sendMessage(messages[2] + " added to tracking list").queue();
+    }
+
+
+    public void removeTrackedUser(String[] messages, MessageReceivedEvent event){
+        String username = "";
+
+        for(int i = 2; i < messages.length; i++){
+            username += messages[i];
+        }
+
+        for(OsuUser user : osuUsersList){
+            if(user.getUsername().equals(username)){
+                osuUsersList.remove(user);
+                writeTrackedUsersJson();
+                event.getChannel().sendMessage("User " + username + " removed from tracking list").queue();
+                return;
+            }
+        }
+        event.getChannel().sendMessage("User " + username + " not found").queue();
+    }
+
+    public void listTrackedUsers(String[] messages, MessageReceivedEvent event){
+
+        String message = "";
+        for(OsuUser user : osuUsersList){
+            message += user.getUsername() + "\n";
+        }
+
+        if(message.equals("")){
+            event.getChannel().sendMessage("No tracked users").queue();
+        }
+        else{
+            event.getChannel().sendMessage(message).queue();
+        }
+    }
+
+    public void writeTrackedUsersJson(){
+        JSONObject usersJson = new JSONObject();
+        JSONArray usersJsonArray = new JSONArray();
+        for(OsuUser user : osuUsersList){
+            usersJsonArray.add(user.getUsername());
+        }
+        usersJson.put("users", usersJsonArray);
+        try (FileWriter file = new FileWriter("resources/OsuUsers.json")) {
+
+            file.write(usersJson.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void osuEvent(String[] messages, MessageReceivedEvent event){
         if(messages[1].equals("add")){
-
-            String username = "";
-
-            for(int i = 2; i < messages.length; i++){
-                username += messages[i];
-            }
-
-            osuUsersList.add(new OsuUser(username));
-            event.getChannel().sendMessage(messages[2] + " added to tracking list").queue();
+            addUserToTracking(messages, event);
         }
         else if(messages[1].equals("list")){
-
-            String message = "";
-            for(OsuUser user : osuUsersList){
-                message += user.getUsername() + "\n";
-            }
-
-            if(message.equals("")){
-                event.getChannel().sendMessage("No tracked users").queue();
-            }
-            else {
-                event.getChannel().sendMessage(message).queue();
-            }
+            listTrackedUsers(messages, event);
+        }
+        else if(messages[1].equals("remove")){
+            removeTrackedUser(messages, event);
         }
     }
 }
