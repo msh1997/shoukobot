e3d56120d6261a51dc365c87473515e2c3c93234
realappdev <realappdev2018@gmail.com>
2019-05-19 21:23:32 -0400

    changed bot prefix

--- src/main/java/Services/MessageParser.java
+++ src/main/java/Services/MessageParser.java
@@ -2,10 +2,6 @@ package Services;
 
 public class MessageParser {
 
-    public MessageParser(){
-
-    }
-
     public String[] parseMessage(String message){
         String[] messages = message.split(" ");
         return messages;
