f48d34d9c8123d9b31e67d06a06e03d0314f2fb9
realappdev <realappdev2018@gmail.com>
2019-05-19 20:11:50 -0400

    adding config file and prefixing

--- src/main/java/Listeners/TestListener.java
+++ src/main/java/Listeners/TestListener.java
@@ -1,14 +1,24 @@
 package Listeners;
 
+import Services.MessageParser;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class TestListener extends ListenerAdapter {
 
+    private MessageParser messageParser = new MessageParser();
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
         System.out.println("Received a message from " + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
-        if(event.getMessage().getContentRaw().equals("!ping"))
-            event.getChannel().sendMessage("Pong!");
+        String[] messages = messageParser.parseMessage(event.getMessage().getContentRaw());
+
+        if(!messages[0].equals(".r")){
+            return;
+        }
+
+        if(messages[1].equals("ping")) {
+            event.getChannel().sendMessage("pong").queue();
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,28 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
 
-    public static void main(String[] args) throws LoginException {
+    public static String token;
+    public static String prefix = "";
+
+    public static void main(String[] args) throws LoginException, IOException {
+        setProperties();
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NTc4Nzc4MzIyODkzNTM3Mjgz.XOHjiA.o_YQk8T-2YmHPBirKLxMOqlea80";
         builder.setToken(token);
         TestListener listener = new TestListener();
         builder.addEventListener(listener);
         builder.buildAsync();
+    }
+
+    public static void setProperties() throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("resources/config.properties"));
+        token = properties.getProperty("token");
+        prefix = properties.getProperty("bot-prefix");
     }
 }
