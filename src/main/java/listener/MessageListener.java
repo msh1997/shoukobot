2fb6a72b3506de6f63f5b0933d20d6539764324a
Ubar <ayu4@lsu.edu>
2019-05-23 20:10:15 -0500

    refactor connect string parsing

--- src/main/java/listener/MessageListener.java
+++ src/main/java/listener/MessageListener.java
@@ -1,8 +1,8 @@
 package listener;
 
-import java.util.regex.*;
 import modules.customReactions.CustomEventHandler;
 import modules.tf2.logs.LogsEventHandler;
+import modules.tf2.ConnectStringParser;
 import modules.misc.MiscEventHandler;
 import modules.osu.OsuEventHandler;
 import services.MessageParser;
@@ -12,11 +12,13 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class MessageListener extends ListenerAdapter {
 
     private MessageParser messageParser = new MessageParser();
+    private ConnectStringParser connectStringParser = new ConnectStringParser();
     private String prefix;
     private OsuEventHandler osuEventHandler;
     private CustomEventHandler customEventHandler;
     private MiscEventHandler miscEventHandler;
     private LogsEventHandler logsEventHandler;
+
 
     public MessageListener(String prefix){
         this.prefix = prefix;
@@ -56,24 +58,7 @@ public class MessageListener extends ListenerAdapter {
 
         if(!messages[0].startsWith(this.prefix)) {
             if (messages[0].equals("connect")) {
-                String content = event.getMessage().getContentRaw();
-                content = content.replaceFirst("https://", "");
-                Pattern connectString = Pattern.compile("(connect) (([a-zA-Z0-9]|\\.)+)\\:[0-9]+(\\;\\s*(password) (.)+)*");
-                Matcher m = connectString.matcher(content);
-                if (m.matches()) {
-                    String info;
-                    String password = messages[messages.length-1];
-                    if (messages.length == 2) {
-                        info = "steam://" + messages[0] + "/" + messages[1];
-                    } else if (messages.length == 3) {
-                        String host = messages[1].replace(";password", "");
-                        info = "steam://" + messages[0] + "/" + host + "/" + password;
-                    } else {
-                        String host = messages[1].substring(0, messages[1].length() - 1);
-                        info = "steam://" + messages[0] + "/" + host + "/" + password;
-                    }
-                    event.getChannel().sendMessage(info).queue();
-                }
+                connectStringParser.connectString(messages, event);
             }
             return;
         }
--- src/main/java/modules/tf2/ConnectStringParser.java
+++ src/main/java/modules/tf2/ConnectStringParser.java
@@ -1,4 +1,30 @@
 package modules.tf2;
 
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ConnectStringParser {
+
+    public void connectString(String[] messages, MessageReceivedEvent event) {
+        String content = event.getMessage().getContentRaw();
+        content = content.replaceFirst("https://", "");
+        Pattern connectString = Pattern.compile("(connect) (([a-zA-Z0-9]|\\.)+)\\:[0-9]+(\\;\\s*(password) (.)+)*");
+        Matcher m = connectString.matcher(content);
+        if (m.matches()) {
+            String info;
+            String password = messages[messages.length-1];
+            if (messages.length == 2) {
+                info = "steam://" + messages[0] + "/" + messages[1];
+            } else if (messages.length == 3) {
+                String host = messages[1].replace(";password", "");
+                info = "steam://" + messages[0] + "/" + host + "/" + password;
+            } else {
+                String host = messages[1].substring(0, messages[1].length() - 1);
+                info = "steam://" + messages[0] + "/" + host + "/" + password;
+            }
+            event.getChannel().sendMessage(info).queue();
+        }
+    }
 }
