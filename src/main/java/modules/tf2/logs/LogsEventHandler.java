7a8bda00b33347f0d994ee6049b513a2e90147c2
Matt <e123matt@gmail.com>
2019-05-23 20:12:03 -0500

    adding users works properly now

--- src/main/java/modules/tf2/logs/LogsEventHandler.java
+++ src/main/java/modules/tf2/logs/LogsEventHandler.java
@@ -1,9 +1,11 @@
 package modules.tf2.logs;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import org.json.simple.parser.JSONParser;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,37 +14,66 @@ import java.util.List;
 public class LogsEventHandler {
 
     List<LogsUser> logsUserList = new ArrayList<>();
-    private JSONParser parser = new JSONParser();
     private ObjectMapper objectMapper = new ObjectMapper();
 
     public LogsEventHandler() {
+        try (FileReader reader = new FileReader("resources/LogsUsers.json"))
+        {
+            //Read JSON file
+            logsUserList = objectMapper.readValue(reader, new TypeReference<List<LogsUser>>(){});
 
+            System.out.println(logsUserList);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void logsEvent(String[] messages, MessageReceivedEvent event) {
         if(messages[1].equals("get")) {
-            // need error checking, like if user doesn't exist
-            event.getChannel().sendMessage("logs event reached").queue();
-            if(messages.length > 2) {
-                event.getChannel().sendMessage("ID Input: " + messages[2]).queue();
-            }
+            getTrackedUser(messages, event);
         }
         else if(messages[1].equals("add")){
             addTrackedUser(messages, event);
+        }
+    }
 
+    public void getTrackedUser(String[] messages, MessageReceivedEvent event) {
+        if(containsName(messages[2])) {
+            event.getChannel().sendMessage("getTrackedUser exists reached.").queue();
+            // TODO: Implement
+        }
+        else {
+            event.getChannel().sendMessage("That user is not being tracked.").queue();
         }
     }
 
     public void addTrackedUser(String[] messages, MessageReceivedEvent event){
 
         LogsUser user = new LogsUser(messages[3], messages[2]);
-        logsUserList.add(user);
 
-        try (FileWriter file = new FileWriter("resources/LogsUsers.json")) {
-            objectMapper.writeValue(file, logsUserList);
-            event.getChannel().sendMessage("User " + messages[2]).queue();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(containsId(messages[3])) {
+            event.getChannel().sendMessage("That Steam ID already exists.").queue();
         }
+        else {
+            logsUserList.add(user);
+
+            try (FileWriter file = new FileWriter("resources/LogsUsers.json")) {
+                objectMapper.writeValue(file, logsUserList);
+                event.getChannel().sendMessage("User \"" + messages[2] + "\" with SteamID64: [" + messages[3] +  "] successfully added").queue();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean containsName(final String name){
+        return logsUserList.stream().map(LogsUser::getUsername).filter(name::equals).findFirst().isPresent();
+    }
+
+    private boolean containsId(final String ID){
+        return logsUserList.stream().map(LogsUser::getSteamId).filter(ID::equals).findFirst().isPresent();
     }
 }
